a2c8eafb53ebd512e4027070b0997738985b34c4
Tim <tim..newton.larsson@outlook.com>
2025-09-18 15:37:00 +0200

    Created Data layer with repositories, entity and DbContext

---
+++ Data/Entities/WorkoutEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities;
+
+public class WorkoutEntity
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    [Required]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    public string Location { get; set; } = null!;
+
+    [Column(TypeName = "datetime2")]
+    [Required]
+    public DateTime StartTime { get; set; }
+
+    [Required]
+    public string Instructor { get; set; } = null!;
+
+}
---
+++ Data/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+using System.Linq.Expressions;
+namespace Data.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task<RepositoryResult> AddAsync(TEntity entity);
+        Task<RepositoryResult> DeleteAsync(TEntity entity);
+        Task<RepositoryResult<IEnumerable<TEntity>>> GetAllAsync();
+        Task<RepositoryResult<TEntity?>> GetAsync(Expression<Func<TEntity, bool>> expression);
+        Task<RepositoryResult> UpdateAsync(TEntity entity);
+    }
+}
