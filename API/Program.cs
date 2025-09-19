466c3af37cf0d1e5c96025ce1e2c73448486772b
Joakim Erik Åkerman <joakim.akerman@hotmail.com>
2025-09-19 11:57:31 +0200

    Added swagger

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Business.Interfaces;
 using Business.Services;
 using Data.Contexts;
 using Data.Interfaces;
@@ -7,27 +8,38 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
-builder.Services.AddScoped<IWorkOutService, WorkoutService>();  
+builder.Services.AddScoped<IWorkoutService, WorkoutService>();  
 builder.Services.AddScoped<IWorkoutRepository, WorkoutRepository>();
 builder.Services.AddControllers();
-builder.Services.AddCors(o =>
-{
-    o.AddPolicy("CorsPolicy", p =>
-    {
-        if (builder.Environment.IsDevelopment())
-            p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
-        else
-            p.WithOrigins("frontend link here", "booking link here")
-             .AllowAnyHeader().AllowAnyMethod().AllowCredentials();
-    });
-});
+
+// CORS Policy when we have a frontend and booking API
+//builder.Services.AddCors(o =>
+//{
+//    o.AddPolicy("CorsPolicy", p =>
+//    {
+//        if (builder.Environment.IsDevelopment())
+//            p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+//        else
+//            p.WithOrigins("frontend API-link here", "booking API-link here")
+//             .AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+//    });
+//});
+
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.MapOpenApi();
-app.UseHttpsRedirection();
 
+app.UseSwagger();
+app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Workout Service API");
+    c.RoutePrefix = string.Empty;
+});
+
+app.UseHttpsRedirection();
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
 //app.UseAuthentication();
 //app.UseAuthorization();
