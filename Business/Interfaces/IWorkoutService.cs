f07637fff54ab1d3723eb92d0473f4a8cdae98f8
Joakim Erik Åkerman <joakim.akerman@hotmail.com>
2025-09-19 11:29:46 +0200

    Added interface

---
+++ Business/Interfaces/IWorkoutService.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using Data.Entities;
+using Domain.Models;
+
+namespace Business.Interfaces
+{
+    public interface IWorkoutService
+    {
+        Task<WorkoutResult> CreateWorkoutAsync(WorkoutCreationRequest request);
+        Task<WorkoutResult<IEnumerable<WorkoutResponseModel>>> GetAllWorkoutsAsync();
+        Task<WorkoutResult<WorkoutResponseModel>> GetWorkoutByExpressionAsync(Expression<Func<WorkoutEntity, bool>> expression);
+        Task<WorkoutResult> UpdateWorkoutAsync(string id, WorkoutCreationRequest updateRequest);
+        Task<WorkoutResult> DeleteEventAsync(string id);
+    }
+}
